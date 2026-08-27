50baf47e9346147cfa75f61b03aef1a32d4a453c
Sidharth Hariharan <64404030+thefundamentaltheor3m@users.noreply.github.com>
2026-08-27 18:28:16 -0400

    Update SpherePacking/ForMathlib/Analysis/Complex/Exponential.lean
    Co-authored-by: Bhavik Mehta <bhavikmehta8@gmail.com>

--- SpherePacking/ForMathlib/Analysis/Complex/Exponential.lean
+++ SpherePacking/ForMathlib/Analysis/Complex/Exponential.lean
@@ -11,30 +11,24 @@ public import Mathlib
 
 namespace Real
 
-theorem exp_decay (k : ℕ) {x : ℝ} (hx : 0 ≤ x) : x ^ k * rexp (-k * x) ≤ 1 := by
-  rw [neg_mul_comm, exp_nat_mul, ← mul_pow x (rexp (-x)) k]
-  refine pow_le_one₀ (by positivity) ?_
-  calc x * rexp (-x)
-    _ ≤ rexp x * rexp (-x) := by
-      gcongr
-      linarith [add_one_le_exp x]
-    _ = 1 := by simp [← exp_add]
-
+-- note the stronger bound (k / r * exp (-1)) ^ k is true as well but we don't need it
+-- if you want it, just remove the last two bits in the grw at the end of this proof
 theorem exp_neg_mul_decay (k : ℕ) {r : ℝ} (hr : 0 < r) {x : ℝ} (hx : 0 ≤ x) :
-    x ^ k * rexp (-r * x) ≤ (k / r) ^ k := by
+    x ^ k * exp (-r * x) ≤ (k / r) ^ k := by
   rcases Nat.eq_zero_or_pos k with rfl | hk
   · simp only [pow_zero, one_mul, Nat.cast_zero]
     exact exp_le_one_iff.2 (by nlinarith)
-  · have hk0 : (k : ℝ) ≠ 0 := Nat.cast_ne_zero.2 hk.ne'
-    calc x ^ k * rexp (-r * x)
-      _ = (k / r) ^ k * ((r * x / k) ^ k * rexp (-k * (r * x / k))) := by
-          rw [← mul_assoc, ← mul_pow]
-          congr 2
-          · field_simp
-          · field_simp
-      _ ≤ (k / r) ^ k * 1 := by
-          have := exp_decay k (x := r * x / k) (by positivity)
-          gcongr (k / r) ^ k * ?_
-      _ = (k / r) ^ k := mul_one _
+  calc 
+    _ = ((r * x / k) * rexp (- (r * x / k)) * (k / r)) ^ k := by
+      rw [mul_right_comm, mul_pow, ← Real.exp_nat_mul]
+      field_simp 
+    _ ≤ (k / r) ^ k := by 
+      grw [Real.mul_exp_neg_le_exp_neg_one, exp_le_one_iff.2 (by simp), one_mul]
+
+theorem exp_decay (k : ℕ) {x : ℝ} (hx : 0 ≤ x) : x ^ k * rexp (-k * x) ≤ 1 := by
+  rcases Nat.eq_zero_or_pos k with rfl | hk
+  · simp only [pow_zero, one_mul, Nat.cast_zero]
+    exact exp_le_one_iff.2 (by nlinarith)
+  grw [exp_neg_mul_decay _ (by positivity) hx, div_self (by positivity), one_pow]
 
 end Real
